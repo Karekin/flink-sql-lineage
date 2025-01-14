9280a194a4c3c010245f2e6a7fcf45f0115b023f
dongrxin <dongrxin@yonyou.com>
2025-01-14 11:23:51 +0800

    doc: 学习血缘项目

--- lineage-loader/src/main/java/com/hw/lineage/loader/classloading/TemporaryClassLoaderContext.java
+++ lineage-loader/src/main/java/com/hw/lineage/loader/classloading/TemporaryClassLoaderContext.java
@@ -1,79 +1,82 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.hw.lineage.loader.classloading;
 
 /**
- * Sets a context class loader in a "try-with-resources" pattern.
+ * 使用 "try-with-resources" 模式设置临时上下文类加载器。
  *
  * <pre>{@code
  * try (TemporaryClassLoaderContext ignored = TemporaryClassLoaderContext.of(classloader)) {
- *     // code that needs the context class loader
+ *     // 需要使用指定上下文类加载器的代码
  * }
  * }</pre>
  *
- * <p>This is conceptually the same as the code below.
+ * <p>这种实现方式与以下代码的功能一致，但更简洁：
  *
  * <pre>{@code
  * ClassLoader original = Thread.currentThread().getContextClassLoader();
  * Thread.currentThread().setContextClassLoader(classloader);
  * try {
- *     // code that needs the context class loader
+ *     // 需要使用指定上下文类加载器的代码
  * } finally {
  *     Thread.currentThread().setContextClassLoader(original);
  * }
  * }</pre>
  *
- * @description: TemporaryClassLoaderContext
+ * @description: TemporaryClassLoaderContext 用于临时更改线程的上下文类加载器。
  * @author: HamaWhite
  */
 public class TemporaryClassLoaderContext implements AutoCloseable {
 
     /**
-     * Sets the context class loader to the given ClassLoader and returns a resource that sets it
-     * back to the current context ClassLoader when the resource is closed.
+     * 设置指定的上下文类加载器，并返回一个资源对象。
+     * 当资源对象被关闭时（例如在 try-with-resources 语句中），会自动将上下文类加载器恢复为原始的类加载器。
      *
+     * 使用示例：
      * <pre>{@code
      * try (TemporaryClassLoaderContext ignored = TemporaryClassLoaderContext.of(classloader)) {
-     *     // code that needs the context class loader
+     *     // 需要使用指定上下文类加载器的代码
      * }
      * }</pre>
+     *
+     * @param cl 要设置的上下文类加载器
+     * @return TemporaryClassLoaderContext 对象，负责在关闭时恢复原始类加载器
      */
     public static TemporaryClassLoaderContext of(ClassLoader cl) {
+        // 获取当前线程
         final Thread t = Thread.currentThread();
+        // 保存当前线程的原始上下文类加载器
         final ClassLoader original = t.getContextClassLoader();
 
+        // 将线程的上下文类加载器设置为指定的类加载器
         t.setContextClassLoader(cl);
 
+        // 返回 TemporaryClassLoaderContext 实例
         return new TemporaryClassLoaderContext(t, original);
     }
 
+    // 当前线程
     private final Thread thread;
 
+    // 原始的上下文类加载器
     private final ClassLoader originalContextClassLoader;
 
+    /**
+     * 私有构造方法，用于创建 TemporaryClassLoaderContext 实例。
+     *
+     * @param thread 当前线程
+     * @param originalContextClassLoader 原始的上下文类加载器
+     */
     private TemporaryClassLoaderContext(Thread thread, ClassLoader originalContextClassLoader) {
         this.thread = thread;
         this.originalContextClassLoader = originalContextClassLoader;
     }
 
+    /**
+     * 恢复线程的上下文类加载器为原始的类加载器。
+     * 此方法会在 try-with-resources 语句块结束时自动调用。
+     */
     @Override
     public void close() {
+        // 将线程的上下文类加载器恢复为原始的类加载器
         thread.setContextClassLoader(originalContextClassLoader);
     }
 }
--- lineage-loader/src/main/java/com/hw/lineage/loader/plugin/manager/PluginManager.java
+++ lineage-loader/src/main/java/com/hw/lineage/loader/plugin/manager/PluginManager.java
@@ -1,42 +1,40 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.hw.lineage.loader.plugin.manager;
 
 import java.util.Iterator;
 import java.util.Map;
 
 /**
- * PluginManager is responsible for managing cluster plugins which are loaded using separate class
- * loaders so that their dependencies don't interfere with Lineage's dependencies.
+ * PluginManager的职责是管理集群中的插件，这些插件是通过独立的类加载器加载的。
+ * 使用独立类加载器的好处是可以避免插件的依赖与本系统（Lineage）的依赖发生冲突。
+ * 换句话说，PluginManager为插件提供了一种隔离加载的机制，使每个插件都可以拥有独立的依赖环境，
+ * 从而降低版本冲突的风险，提高系统的模块化与可维护性。
  *
- * @description: PluginManager
- * @author: HamaWhite
+ * @description: PluginManager接口定义了与插件管理相关的方法，由实现类负责提供具体的插件加载与管理逻辑。
+ *              一般在集群环境下使用，以便动态加载、卸载或查找特定类型（SPI服务接口）的插件实现。
+ *
+ * author: HamaWhite
  */
 public interface PluginManager {
 
     /**
-     * Returns in pluginId->iterator over all available implementations of the given service interface (SPI)
-     * in echo plugin known to this plugin manager instance.
+     * 根据给定的服务接口（即SPI：Service Provider Interface），为所有已知的插件返回其实现该服务的实例迭代器。
      *
-     * @param service the service interface (SPI) for which implementations are requested.
-     * @param <P>     Type of the requested plugin service.
-     * @return Map<pluginId, iterator>
+     * 执行过程和意义：
+     * 1. service参数是一个接口或抽象类，它是插件需要实现的服务接口（SPI）。调用此方法时，调用者希望获得
+     *    系统中所有插件对该SPI的实现。
+     * 2. 方法返回一个Map，键为pluginId（插件ID），值为一个Iterator<P>，用于遍历该插件提供的所有SPI实现类实例。
+     * 3. 每个插件都由独立的类加载器加载，所以不同插件间的SPI实现是相互隔离的。但通过此方法，用户能一次性获取
+     *    所有插件中对该SPI的实现列表，方便对比、选择或统一处理。
+     *
+     * 类型参数：
+     * @param <P>    表示所请求的SPI服务接口的类型。例如，如果service是一个接口PluginService，那么<P>就是PluginService。
+     *
+     * 参数描述：
+     * @param service 要查找实现类的服务接口Class对象。此参数用于反射或SPI机制来从插件中加载实现类。
+     *
+     * 返回值：
+     * @return 一个Map，其中键为插件ID（字符串），值为一个Iterator<P>对象，用于遍历该插件提供的所有P类型的实现实例。
+     *         如果某个插件不提供此SPI的实现，那么该插件不会出现在返回的Map中。
      */
     <P> Map<String, Iterator<P>> load(Class<P> service);
 
